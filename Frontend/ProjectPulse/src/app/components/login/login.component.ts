e7b254c1d3ccd21e368435ef66f387d88682a0fa
Sheilla-Chomba <chombasheila980@gmail.com>
2024-02-16 11:36:31 +0300

    Login Routes

--- Frontend/ProjectPulse/src/app/components/login/login.component.ts
+++ Frontend/ProjectPulse/src/app/components/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private route: Router) {}
 
+  navigateToRegister(){
+    this.route.navigate(["register"]);
+  }
 }
